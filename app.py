e96df383c544543c7c66c84067224d6b721132ab
tmuraoka0113 <taihei1324@gmail.com>
2024-12-14 17:00:41 +0900

    コミット

--- app.py
+++ app.py
@@ -1,6 +1,95 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from peewee import IntegrityError
+from config import User
 
 app = Flask(__name__)
+app.secret_key = "secret"  # 秘密の鍵を設定
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
+
+
+# ログインしていないとアクセスできないページにアクセスがあった場合の処理
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return redirect(url_for("login"))
+
+
+# ユーザー登録フォームの表示・登録処理
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # 入力データの検証(未入力)
+        if not request.form["name"] or not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります")
+            return redirect(request.url)
+
+        # 入力データの検証(重複)
+        if User.select().where(User.name == request.form["name"]):
+            flash("その名前はすでに使用されています")
+            return redirect(request.url)
+        if User.select().where(User.name == request.form["email"]):
+            flash("そのメールアドレスはすでに使用されています")
+            return redirect(request.url)
+
+        # ユーザー登録処理
+        try:
+            # ユーザー登録
+            User.create(
+                name=request.form["name"],
+                email=request.form["email"],
+                password=generate_password_hash(request.form["password"]),
+            )
+            return render_template("index.html")
+        except IntegrityError as e:
+            flash(f"{e}")
+
+    return render_template("register.html")
+
+
+# ログインフォームの表示・ログイン処理
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # データの検
+        if not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+
+        # ここでユーザーを認証し、OKならログインする
+        user = User.select().where(User.email == request.form["email"]).first()
+        if user is not None and check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            flash(f"ようこそ！ {user.name} さん")
+            return redirect(url_for("index"))
+        # NGならフラッシュメッセージを設定
+        flash("認証に失敗しました")
+
+    return render_template("login.html")
+
+
+# ログアウト処理
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("ログアウトしました！")
+    return redirect(url_for("index"))
+
+
+# ユーザー削除処理
+@app.route("/unregister")
+@login_required
+def unregister():
+    current_user.delete_instance()
+    logout_user()
+    return redirect(url_for("index"))
 
 
 @app.route("/")
---
+++ config.py
@@ -0,0 +1,18 @@
+from peewee import SqliteDatabase, Model, IntegerField, CharField, TextField
+from flask_login import UserMixin
+
+db = SqliteDatabase("db.sqlite")
+
+
+class User(UserMixin, Model):
+    id = IntegerField(primary_key=True)
+    name = CharField(unique=True)
+    email = CharField(unique=True)
+    password = TextField()
+
+    class Meta:
+        database = db
+        table_name = "users"
+
+
+db.create_tables([User])
