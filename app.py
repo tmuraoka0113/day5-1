f96a608a011d19e052057bf0a77d1ae52b795bb0
tmuraoka0113 <taihei1324@gmail.com>
2024-12-15 10:54:20 +0900

    完成　

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
+from config import User, Message
 from peewee import IntegrityError
-from config import User
 
+# 初期設定的なやつ
 app = Flask(__name__)
-app.secret_key = "secret"  # 秘密の鍵を設定
+app.secret_key = "secret"  # 秘密鍵を設定
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+# ログインユーザーの情報を取得できるようにする
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
@@ -21,26 +23,24 @@ def unauthorized_handler():
     return redirect(url_for("login"))
 
 
-# ユーザー登録フォームの表示・登録処理
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # 入力データの検証(未入力)
+        # 入力データの検証:未入力の確認
         if not request.form["name"] or not request.form["password"] or not request.form["email"]:
             flash("未入力の項目があります")
             return redirect(request.url)
 
-        # 入力データの検証(重複)
+        # 入力データの検証:重複の確認
         if User.select().where(User.name == request.form["name"]):
-            flash("その名前はすでに使用されています")
+            flash("その名前はすでに使われています。")
             return redirect(request.url)
-        if User.select().where(User.name == request.form["email"]):
-            flash("そのメールアドレスはすでに使用されています")
+        if User.select().where(User.email == request.form["email"]):
+            flash("そのメールアドレスはすでに使われています。")
             return redirect(request.url)
 
         # ユーザー登録処理
         try:
-            # ユーザー登録
             User.create(
                 name=request.form["name"],
                 email=request.form["email"],
@@ -53,23 +53,23 @@ def register():
     return render_template("register.html")
 
 
-# ログインフォームの表示・ログイン処理
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # データの検
+        # 入力のチェック
         if not request.form["password"] or not request.form["email"]:
-            flash("未入力の項目があります。")
+            flash("未入力の項目があります")
             return redirect(request.url)
 
-        # ここでユーザーを認証し、OKならログインする
+        # 認証してOKならログイン
         user = User.select().where(User.email == request.form["email"]).first()
         if user is not None and check_password_hash(user.password, request.form["password"]):
             login_user(user)
-            flash(f"ようこそ！ {user.name} さん")
+            flash(f"ようこそ！{user.name}さん")
             return redirect(url_for("index"))
-        # NGならフラッシュメッセージを設定
-        flash("認証に失敗しました")
+
+        # だめだったらフラッシュメッセージ
+        flash("認証に失敗しました。")
 
     return render_template("login.html")
 
@@ -83,7 +83,7 @@ def logout():
     return redirect(url_for("index"))
 
 
-# ユーザー削除処理
+# ユーザー削除
 @app.route("/unregister")
 @login_required
 def unregister():
@@ -92,9 +92,48 @@ def unregister():
     return redirect(url_for("index"))
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST" and current_user.is_authenticated:
+        Message.create(user=current_user, content=request.form["content"])
+    messages = (
+        Message.select()
+        .where(Message.reply_to.is_null(True))
+        .order_by(Message.pub_date.desc(), Message.id.desc())
+    )
+    return render_template("index.html", messages=messages)
+
+
+# メッセージ削除
+@app.route("/messages/<message_id>/delete/", methods=["POST"])
+@login_required
+def delete(message_id):
+    if Message.select().where((Message.id == message_id) & (Message.user == current_user)).first():
+        Message.delete_by_id(message_id)
+    else:
+        flash("無効な操作です")
+    return redirect(request.referrer)
+
+
+# 返信表示
+@app.route("/messages/<message_id>/")
+def show(message_id):
+    messages = (
+        Message.select()
+        .where((Message.id == message_id) | (Message.reply_to == message_id))
+        .order_by(Message.pub_date.desc())
+    )
+    if messages.count() == 0:
+        return redirect(url_for("index"))
+    return render_template("show.html", messages=messages, message_id=message_id)
+
+
+# 返信登録
+@app.route("/messages/<message_id>/", methods=["POST"])
+@login_required
+def reply(message_id):
+    Message.create(user=current_user, content=request.form["content"], reply_to=message_id)
+    return redirect(url_for("show", message_id=message_id))
 
 
 if __name__ == "__main__":
